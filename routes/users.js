5d12b17e4c0e876bf1e9f0293080a21f6b5c9c11
Jack Ma <jackma1206@gmail.com>
2019-03-11 18:59:13 -0400

    fix landing page

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const router = express.Router();
 
 //load user model
-require('../models/User');
-const User = mongoose.model('users');
-
+require("../models/User");
+const User = mongoose.model("users");
 
 //login form
-router.post('/login', (req, res, next)=>{
-  passport.authenticate('local', {
-    successRedirect:'/posts/index',
-    failureRedirect:'/',
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/posts/index",
+    failureRedirect: "/",
     failureFlash: true
   })(req, res, next);
 });
 
 //register form
-router.post('/register', (req, res)=>{
+router.post("/register", (req, res) => {
   let errors = [];
-  if (req.body.password !== req.body.password2){
+  if (req.body.password !== req.body.password2) {
     errors.push({
       text: "Passwords do not match"
     });
   }
   if (req.body.password.length < 4) {
     errors.push({
-      text: 'Password must be atleast 4 characters'
+      text: "Password must be atleast 4 characters"
     });
   }
-  if(errors > 0){
-    res.render('/', {
+  if (errors > 0) {
+    res.render("/", {
       errors: errors,
       name: req.body.name,
       email: req.body.email,
@@ -40,45 +39,48 @@ router.post('/register', (req, res)=>{
       password2: req.body.password2,
       image: req.body.image
     });
-  }else{
+  } else {
     User.findOne({
       email: req.body.email
-    }).then(user=>{
-      if (user){
-        // req.flash("error_msg", "Email already registered");
-        res.redirect('/');
-      }else{
+    }).then(user => {
+      if (user) {
+        req.flash("error_msg", "Email already registered");
+        res.redirect("/");
+      } else {
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
           password: req.body.password,
           pic: req.body.pic
         });
-        bcrypt.genSalt(10, (err, salt) =>{
-          bcrypt.hash(newUser.password, salt, (err, hash)=>{
-            if(err){
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
               console.log(err);
-            }else{
+            } else {
               newUser.password = hash;
-              newUser.save().then(user=>{
-                res.redirect('/');
-              }).catch(err =>{
-                console.log(err);
-                return;
-              });
+              newUser
+                .save()
+                .then(user => {
+                  res.redirect("/");
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                });
             }
           });
         });
       }
     });
   }
+  res.redirect("/signin");
 });
 
-
 //logout
-router.get('/logout', (req, res)=>{
+router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
